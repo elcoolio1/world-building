c633a89af3ba8f06798cef193236afeada261ade
Ian <imac1844@gmail.com>
2020-08-13 16:11:11 -0700

    Beginning of nextTile
    Started working on the nextTile function, changed neighbour counting into a function, initProb()

--- hexmap.py
+++ hexmap.py
@@ -24,25 +24,41 @@ probE = [0,0,0,0,0,0]
 probMap = [probA,probB,probC,probD,probE]
 
 
-
 # Calculates probability for next tile. Currently the whole board is recalculated for every call, could we make it update only locally?
-# This should probably be a function called by whatever actually decides what tile should be next.
-for i in range (1,len(hexMap)-1): # Range is this way to account for the buffer layer.
-	for j in range (1,len(hexMap[i])-1):
-		if hexMap [i][j] == 1: #If a tile is there in hexMap, that tile in probMap is set to 7
-			probMap[i][j] = 7
-		# Because of the way coordinates work in a hex map, the numerical value of adjacent tiles differes in odd and even rows. This bit
-		# calculates how many adjacent tiles are "full", by summing their values (since full tiles are 1 and empty are 0). The probability
-		# for the next tile should be based on these values 1-6, with 7 being a tile that already exists.
-		elif i%2 == 0:
-			probMap [i][j] = (hexMap[i-1][j-1] + hexMap[i-1][j] + hexMap[i][j-1] + hexMap[i][j+1] + hexMap[i+1][j-1] + hexMap[i+1][j])
-		else:
-			probMap [i][j] = (hexMap[i-1][j] + hexMap[i-1][j+1] + hexMap[i][j-1] + hexMap[i][j+1] + hexMap[i+1][j] + hexMap[i+1][j+1])
+def initProb():
+	global hexMap, probMap
+	for i in range (1,len(hexMap)-1): # Range is this way to account for the buffer layer.
+		for j in range (1,len(hexMap[i])-1):
+			if hexMap [i][j] == 1: #If a tile is there in hexMap, that tile in probMap is set to 7
+				probMap[i][j] = 7
+			# Because of the way coordinates work in a hex map, the numerical value of adjacent tiles differes in odd and even rows. This bit
+			# calculates how many adjacent tiles are "full", by summing their values (since full tiles are 1 and empty are 0). The probability
+			# for the next tile should be based on these values 1-6, with 7 being a tile that already exists.
+			elif i%2 == 0:
+				probMap [i][j] = (hexMap[i-1][j-1] + hexMap[i-1][j] + hexMap[i][j-1] + hexMap[i][j+1] + hexMap[i+1][j-1] + hexMap[i+1][j])
+			else:
+				probMap [i][j] = (hexMap[i-1][j] + hexMap[i-1][j+1] + hexMap[i][j-1] + hexMap[i][j+1] + hexMap[i+1][j] + hexMap[i+1][j+1])
+
+
+# Picks the next tile
+def nextTile():
+	tileCount = 0
+	global hexMap, probMap
+	for i in range(len(probMap)):
+		for j in range (len(probMap[i])):
+			if probMap [i][j] == 7:
+				pass
+			else:
+				tileCount = tileCount + probMap [i][j]
+
+
+
+
 
 
 # TODO: something that uses the xorshift function to decide what tile to place, somehow wieghted by the value 1-6 of the array probMap
 
-
+initProb()
 # Prints the arrays properly
 def printMaps():
 	for i in range (len(probMap)):
@@ -59,4 +75,4 @@ def printMaps():
 		else:
 			print(i, "  ", hexMap[i])
 
-printMaps()
+nextTile()

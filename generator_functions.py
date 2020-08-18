7ef720ae12bf6112bb4720a0d0c3675c6e784f5b
Jack <jack@oomovers.com>
2020-08-18 16:18:50 -0700

    pretty gradients

--- generator_functions.py
+++ generator_functions.py
@@ -3,29 +3,37 @@ from hex_functions import *
 
 
 
-def ax_center_noise(axial,freq):
+
+def ax_center_noise(axial,freq=0.1):
 	pixel = ax2px(axial)
-	x = pixel[0]*100
-	y = pixel[1]*100
+	x = pixel[0]
+	y = pixel[1]
 	gen = OpenSimplex()
 	noise = gen.noise2d(x*freq,y*freq)/2 + 0.5 #/2+.5 moves values into 0 ... 1 from -1 ... 1
 	return noise
 
-def px_noise(pixel,freq):
-	x = pixel[0]*100
-	y = pixel[1]*100
+def px_noise(pixel,freq=0.1):
+	x = pixel[0]
+	y = pixel[1]
 	gen = OpenSimplex()
 	noise = gen.noise2d(x*freq,y*freq)/2 + 0.5 #/2+.5 moves values into 0 ... 1 from -1 ... 1
 	return noise
 
-def elev_color(xy):
-	freq = .001
-	elev1 = px_noise(xy,freq)
-	elev2 = 0.5*px_noise(xy,freq*2)
-	elev3 = 0.25*px_noise(xy,freq*4)
-	elev = elev1+elev2+elev3
-	print(elev)
+def elev(xy,freq=0.1,octaves=3):
+	elev = 0
+	max_elev = 0
+	for i in range(octaves):
+		elev = elev + (1/2**i)*px_noise(xy,freq*(2**i))
+		max_elev = max_elev + (1/2**i)
+	elev = elev/max_elev
 
+	return elev
+
+def elev_color(xy,freq=0.1):
+
+
+	elevation = elev(xy,freq)
+	
 	#water to beach colors
 	deep_water = (0, 129, 175)
 	mid_water  = (0, 171, 231)
@@ -41,25 +49,84 @@ def elev_color(xy):
 
 	dh = 0.1
 
-	if elev < dh*3:
+	if elevation < dh*3:
 		color = deep_water
-	elif elev < dh*3.5:
+	elif elevation < dh*3.5:
 		color = mid_water
-	elif elev < dh*4:
+	elif elevation < dh*4:
 		color = shallows
-	elif elev < dh*4.5:
+	elif elevation < dh*4.5:
 		color = shore
-	elif elev < dh*5:
+	elif elevation < dh*5:
 		color = beach
-	elif elev < dh*7:
+	elif elevation < dh*7:
 		color = undergrowth
-	elif elev < dh*8:
+	elif elevation < dh*8:
 		color = boreal
-	elif elev < dh*10:
+	elif elevation < dh*10:
 		color = evergreen
-	elif elev < dh*10.5:
+	elif elevation < dh*10.5:
 		color = rock
 	else:
 		color = snow
 		
 	return color
+
+def elev_color_sections(xy,freq=.001,break_points=[0.45,0.5,0.7,0.75]):
+	e = elev(xy)
+	sea_level = break_points[0]
+	beach_line = break_points[1]
+	tree_line = break_points[2]
+	snow_line = break_points[3]
+
+	dsea = sea_level-0
+	dbeach = beach_line-sea_level
+	dtree = tree_line-beach_line
+	drock = snow_line-tree_line
+	dsnow = 1-snow_line
+
+
+	dark_blue = 5,18,64
+	light_blue = 45, 199, 255
+
+	shore = 234, 210, 172
+	beach = 234, 186, 107
+
+	undergrowth= 23, 195, 103
+	evergreen  = 22, 67, 35
+
+	light_rock = 103, 98, 92
+	dark_rock = 170, 167, 161
+
+	off_white = 240, 244, 246
+	pure_white = 255,255,255
+
+
+	if e < sea_level:
+		color = color_grad(light_blue,dark_blue,(sea_level-e)/dsea)
+	elif e < beach_line:
+		color = color_grad(beach,shore,(e-sea_level)/dbeach)
+	elif e < tree_line:
+		color = color_grad(undergrowth,evergreen,(e-beach_line)/dtree)
+	elif e < snow_line:
+		color = color_grad(light_rock,dark_rock,(e-tree_line)/drock)
+	else:
+		color = color_grad(off_white,pure_white,(e-snow_line)/dsnow)
+
+	return color
+
+		
+
+
+
+def color_grad(map1,map2,percent):
+	percent = clamp(percent,0,1)
+	red = map1[0]+(map2[0]-map1[0])*percent
+	green = map1[1]+(map2[1]-map1[1])*percent
+	blue = map1[2]+(map2[2]-map1[2])*percent
+
+	color = int(red),int(green),int(blue)
+	return color
+
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
--- tests.py
+++ tests.py
@@ -4,38 +4,86 @@ from generator_functions import *
 import arcade
 
 
+
 scale = 20  # size of hexes
 size = (66, 66)  # number of hexes
 
+# cluster_points = [(0, 0), (2, -5), (5, -3), (3, 2), (-2, 5), (-3, -2), (-5, 3), (1, 7), (-7, 8),
+#                   (-8, 1), (-1, -7), (7, -8), (-6, -4), (6, 4), (8, -1), (10, -6), (4, -10), (-10, 6), (-4, 10)]
+
+
 display_init(scale, size)
-
-
 arcade.start_render()
 
-
-cluster_points = [(0, 0), (2, -5), (5, -3), (3, 2), (-2, 5), (-3, -2), (-5, 3), (1, 7), (-7, 8),
-                  (-8, 1), (-1, -7), (7, -8), (-6, -4), (6, 4), (8, -1), (10, -6), (4, -10), (-10, 6), (-4, 10)]
-
-# def random_color_range(min,max):
-# 	color = (random.randint(min,max),random.randint(min,max),random.randint(min,max))
-# 	# print(color)
-# 	return color
-
-# for i in range(len(cluster_points)):
-# 	color = random_color_range(0,25)
-# 	draw_cluster(cluster_points[i],color,2,False)
-# 	draw_cluster_center(cluster_points[i],white,.5,True)
-
-
-# grid
 for q in range(-60, 60):
     for r in range(-60, 60):
         if ax_distance((q, r), (0, 0)) < 30:
             loc = (q, r)
-            # draw_hex_outline(loc)
-            draw_hex(loc, elev_color(loc))
-            draw_hex_outline(loc, (150, 150, 150))
+            color = elev_color_sections(loc)
+            draw_hex(loc, color)
+            # elevs.append(elev(loc))
+            # draw_hex_outline(loc, (150, 150, 150))
+
+
+# loc = px2ax((-30,30))
+
+# dark_blue = 5,18,64
+# light_blue = 45, 199, 255
+
+# shore = 234, 210, 172
+# beach = 234, 186, 107
+
+# undergrowth= 23, 195, 103
+# evergreen  = 22, 67, 35
+
+# light_rock = 103, 98, 92
+# dark_rock = 170, 167, 161
+
+# off_white = 240, 244, 246
+# pure_white = 255,255,255
+
+
+# color = color_grad(light_blue,dark_blue,0)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+# color = color_grad(light_blue,dark_blue,1)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+
+
+
+# color = color_grad(beach,shore,0)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+# color = color_grad(beach,shore,1)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+
+# color = color_grad(undergrowth,evergreen,0)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+# color = color_grad(undergrowth,evergreen,1)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+
+# color = color_grad(light_rock,dark_rock,0)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+# color = color_grad(light_rock,dark_rock,1)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+
+# color = color_grad(off_white,pure_white,0)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+# color = color_grad(off_white,pure_white,1)
+# draw_hex(loc, color)
+# loc = loc[0]+1,loc[1]
+
 
 
 arcade.finish_render()
 arcade.run()
+
+print(max(elevs))
+print(min(elevs))

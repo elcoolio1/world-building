cd61eddda00de808232b4c06034344a32d71ff8b
Jack <jack@oomovers.com>
2020-08-21 14:14:44 -0700

    works with moving viewport

--- arcade_testing.py
+++ arcade_testing.py
@@ -3,27 +3,26 @@ from hex_functions import *
 from generator_functions import *
 
 
+
+
+window_size = width,height = 1000,1000
+
 hx_scale=20
 grid_thickness=1
-# hx_width = 66
-# hx_height = 66
-px_size = width, height = 50*hx_scale, 50*hx_scale
 
-global origin_X
-origin_x = int(width / 2)
-global origin_y
-origin_y = int(height / 2)
-global origin
-origin = (origin_x, origin_y)
+
+# global origin_X
+# origin_x = int(width / 2)
+# global origin_y
+# origin_y = int(height / 2)
+# global origin
+# origin = (origin_x, origin_y)
 global zoom
 zoom = 0.0001
 
 px_grid_thickness = int(grid_thickness)  # thickness of lines around hex
 
-
-
-
-global_display(hx_scale,origin_x,origin_y)
+global_display(hx_scale,0,0)
 
 
 
@@ -56,14 +55,13 @@ class MyGame(arcade.Window):
 	def __init__(self):
 		""" Initializer """
 		# Call the parent class initializer
-		super().__init__(width, height, "Fucking Work")
-
-
+		super().__init__(width, height, "Map Viewer")
 
 		# Variables that will hold sprite lists
 		self.hex_list = []
-		self.cursor = cursor(origin_x, origin_y, 5, arcade.color.WHITE)
+		self.cursor = cursor(0, 0, 5, arcade.color.WHITE)
 		self.set_mouse_visible(False)
+		# self.viewport = arcade.set_viewport()
 		# self.draw_map()
 		global dragging
 		dragging = False
@@ -73,32 +71,37 @@ class MyGame(arcade.Window):
 
 
 	def draw_map(self):
+		# global origin_y
+		# global origin_x
+		# global origin
+		self.hex_list = arcade.SpriteList(use_spatial_hash=False,)
+		hx_radius = 24
+		vp = arcade.get_viewport()
+		origin_ax=round_ax(px2ax((vp[0]+width/2,vp[2]+height/2)))
 
-		self.new_list = arcade.SpriteList(use_spatial_hash=False,is_static=True)
-		for i in range(len(self.hex_list)):
-			sprite = self.hex_list[i]
-			coord_px = sprite._get_position()
 
-		# for q in range(-30,30):
-		# 	for r in range(-30,30):
-		# 		if ax_distance((q, r), (0,0)) < hx_radius:
-					
-		# 			coord_px = ax2px((q,r))
+		for q in range(origin_ax[0]-40,origin_ax[0]+40):
+			for r in range(origin_ax[1]-40,origin_ax[1]+40):
+				coord_px = ax2px((q,r))
+				# vp = [origin_x-width/2,origin_x+width/2,origin_y-height/2,origin_y+height/2]
+				# vp = arcade.get_viewport()
 
-			elevation_xy=[]
-			for i in range(0,2):
-				elevation_xy.append(coord_px[i]+origin[i])
-			global zoom
-			color = elev_color_sections(elevation_xy,zoom,[0.1,0.15,0.5,0.7])
-			# color = 50,50,50
-			hx = hexagon('hexagon.png',(hx_scale*1.2/(100))) #25% scaling
-			hx.center_x = coord_px[0]
-			hx.center_y = coord_px[1]
-			hx.color = color 
+				if coord_px[0] >= vp[0]-100 and coord_px[0] <= vp[1]+100:
+					if coord_px[1] >= vp[2]-100 and coord_px[1] <= vp[3]+100:
+						
+						# coord_px = ax2px((q,r))
+		
+						
+						global zoom
 
-			self.new_list.append(hx)
-		self.hex_list = arcade.SpriteList(use_spatial_hash=False,is_static=True)
-		self.hex_list = self.new_list
+						color = elev_color_sections(coord_px,zoom,[0.1,0.15,0.5,0.7])
+						hx = hexagon('hexagon.png',(hx_scale*1.2/(100))) #25% scaling
+						hx.center_x = coord_px[0]
+						hx.center_y = coord_px[1]
+						hx.color = color
+
+						self.hex_list.append(hx)
+
 
 
 
@@ -106,26 +109,36 @@ class MyGame(arcade.Window):
 		""" Set up the game and initialize the variables. """
 
 		# Sprite lists
-		self.hex_list = arcade.SpriteList(use_spatial_hash=False,is_static=True)
+		# screen_origin_x = 0
+		# screen_origin_y = 0
+		# arcade.set_viewport(origin_x-width/2,origin_x+width/2,origin_y-height/2,origin_y+height/2)
+		self.hex_list = arcade.SpriteList(use_spatial_hash=False,)
 		hx_radius = 24
-		for q in range(-30,30):
-			for r in range(-30,30):
-				if ax_distance((q, r), (0,0)) < hx_radius:
-					
-					coord_px = ax2px((q,r))
-	
-					elevation_xy=[]
-					for i in range(0,2):
-						elevation_xy.append(coord_px[i]+origin[i])
-					global zoom
+		vp = arcade.get_viewport()
+		origin_ax=round_ax(px2ax((vp[0]+width/2,vp[2]+height/2)))
 
-					color = elev_color_sections(elevation_xy,zoom,[0.1,0.15,0.5,0.7])
-					hx = hexagon('hexagon.png',(hx_scale*1.2/(100))) #25% scaling
-					hx.center_x = coord_px[0]
-					hx.center_y = coord_px[1]
-					hx.color = color
 
-					self.hex_list.append(hx)
+		for q in range(origin_ax[0]-40,origin_ax[0]+40):
+			for r in range(origin_ax[1]-40,origin_ax[1]+40):
+				coord_px = ax2px((q,r))
+				# vp = [origin_x-width/2,origin_x+width/2,origin_y-height/2,origin_y+height/2]
+				# vp = arcade.get_viewport()
+
+				if coord_px[0] >= vp[2] and coord_px[0] <= vp[3]:
+					if coord_px[1] >= vp[0] and coord_px[1] <= vp[1]:
+						
+						# coord_px = ax2px((q,r))
+		
+						
+						global zoom
+
+						color = elev_color_sections(coord_px,zoom,[0.1,0.15,0.5,0.7])
+						hx = hexagon('hexagon.png',(hx_scale*1.2/(100))) #25% scaling
+						hx.center_x = coord_px[0]
+						hx.center_y = coord_px[1]
+						hx.color = color
+
+						self.hex_list.append(hx)
 
 
 	def on_draw(self):
@@ -153,26 +166,36 @@ class MyGame(arcade.Window):
 		self.cursor.position_x = x
 		self.cursor.position_y = y
 		global dragging
-		dx_ax = ax2px(round_ax(px2ax((dx,dy))))
+		# dx_ax = ax2px(round_ax(px2ax((dx,dy))))
 		if dragging:
-			global origin_x
-			origin_x = origin_x-dx#_ax[0]#hx_scale
-			
-			global origin_y
-			origin_y = origin_y-dy#x_ax[1]#hx_scale
-			
-			global origin
-			origin = origin_x,origin_y
-
-			rounded_origin = ax2px(round_ax(px2ax(origin)))
-			origin_x = rounded_origin[0]
-			origin_y = rounded_origin[1]
-
-			origin = origin_x,origin_y
+			# vp = arcade.get_viewport()
+			# new_vp = [vp[0]-dx,vp[1]-dx,vp[2]-dy,vp[3]-dy]
+			vp = arcade.get_viewport()
+			arcade.set_viewport(vp[0]-dx,vp[1]-dx,vp[2]-dy,vp[3]-dy)
 			
 
 
-			print(origin)
+
+
+
+			# global origin_x
+			# origin_x = origin_x-dx#_ax[0]#hx_scale
+			
+			# global origin_y
+			# origin_y = origin_y-dy#x_ax[1]#hx_scale
+			
+			# global origin
+			# origin = origin_x,origin_y
+
+			# rounded_origin = ax2px(round_ax(px2ax(origin)))
+			# origin_x = rounded_origin[0]
+			# origin_y = rounded_origin[1]
+
+			# origin = origin_x,origin_y
+			
+
+
+			# print(origin)
 
 	def on_mouse_scroll(self,x,y,scroll_x,scroll_y):
 		global zoom
@@ -181,9 +204,11 @@ class MyGame(arcade.Window):
 
 
 	def update(self, delta_time):
+
 		
 		self.hex_list.update()
 		self.draw_map()
+
 
 		
 
